11400c56f6cc7675d5c74962d0c8f63604f2a615
Duc Nguyen <mingduc17082002@gmail.com>
2022-11-27 20:05:52 +0700

    Add gender into user, add status into checkout

--- backend/models/checkoutModel.js
+++ backend/models/checkoutModel.js
@@ -38,6 +38,10 @@ const checkoutSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    status: {
+      type: String,
+      enum: ["wating", "delivering", "delivered"],
+    },
     note: {
       type: String,
     },
