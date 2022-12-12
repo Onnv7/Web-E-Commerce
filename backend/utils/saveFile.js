efc836174e4b5b95b1f131f8a39eb73e7b311d61
Nguyễn Văn An <nva6112002@gmail.com>
2022-12-12 09:41:40 +0700

    Update Product CRUD

--- backend/utils/saveFile.js
+++ backend/utils/saveFile.js
@@ -4,28 +4,30 @@ export function saveSingleFile(doc, coverEncoded) {
     if (coverEncoded == null) return
 
     var img = [];
-    const cover = JSON.parse(coverEncoded)
-    if (cover != null && imageMimeTypes.includes(cover.type)) {
-        var data = {
-            coverImage: new Buffer.from(cover.data, 'base64'),
-            coverImageType: cover.type
-        }
-        img.push(data);
-    }
+    var data = getDataForImage(coverEncoded);
+    img.push(data);
     doc.img = img;
 }
 export function saveMultipleFile(doc, coverEncoded) {
     if (coverEncoded == null) return
     var i = 0;
     var img = [];
+    var data = {};
     for (i = 0; i < coverEncoded.length; i++) {
-        const cover = JSON.parse(coverEncoded[i])
-        var data = {}
-        data = {
-            coverImage: new Buffer.from(cover.data, 'base64'),
-            coverImageType: cover.type
-        }
+        var data = getDataForImage(coverEncoded[i]);
         img.push(data);
     }
     doc.img = img;
+}
+// convert from encoded image to data img
+export function getDataForImage(coverEncoded) {
+    const cover = JSON.parse(coverEncoded);
+    var data;
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+        data = {
+            coverImage: new Buffer.from(cover.data, "base64"),
+            coverImageType: cover.type,
+        };
+    }
+    return data;
 }
