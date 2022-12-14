8fcdff97c4f4eb9bf818d2f7a1bf50dd99f9b841
Duc Nguyen <mingduc17082002@gmail.com>
2022-12-14 14:34:32 +0700

    Finish wishlist

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import GlobalStyles from './Components/GlobalStyles';
-import reportWebVitals from './reportWebVitals';
-import { AuthContextProvider } from './context/AuthContext.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import GlobalStyles from "./Components/GlobalStyles";
+import reportWebVitals from "./reportWebVitals";
+import { AuthContextProvider } from "./context/AuthContext.js";
+import { StoreProvider } from "./context/StoreContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <GlobalStyles>
             <AuthContextProvider>
-                <App />
+                <StoreProvider>
+                    <App />
+                </StoreProvider>
             </AuthContextProvider>
         </GlobalStyles>
-    </React.StrictMode>,
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
