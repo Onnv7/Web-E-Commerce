09ca9585eab70ba3c445620fe4d5c1a0d7f81703
Duc Nguyen <mingduc17082002@gmail.com>
2022-12-15 02:14:17 +0700

    Fix model

--- backend/models/shopModel.js
+++ backend/models/shopModel.js
@@ -1,56 +1,52 @@
 import mongoose, { mongo } from "mongoose";
 
 const shopSchema = new mongoose.Schema(
-  {
-    name: {
-      required: true,
-      type: String,
-    },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: "User",
-      required: true,
-      unique: true,
-    },
-    followerQuantity: {
-      type: Number,
-      default: 0,
-    },
-    ratingAverage: {
-      type: Number,
-      default: 0,
-      min: [0, "Rating must be above 1"],
-      max: [5, "Rating must be below 5"],
-    },
-    ratingQuantity: { type: Number, default: 0 },
-    subCategory: {
-      type: [
-        {
-          name: {
+    {
+        name: {
+            required: true,
             type: String,
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
             required: true,
             unique: true,
-          },
-          quantity: {
+        },
+        ratingAverage: {
             type: Number,
             default: 0,
-          },
+            min: [0, "Rating must be above 1"],
+            max: [5, "Rating must be below 5"],
         },
-      ],
-      required: false,
-      default: [
-        {
-          name: "All",
-          quantity: 0,
+        ratingQuantity: { type: Number, default: 0 },
+        subCategory: {
+            type: [
+                {
+                    name: {
+                        type: String,
+                        required: true,
+                        unique: true,
+                    },
+                    quantity: {
+                        type: Number,
+                        default: 0,
+                    },
+                },
+            ],
+            required: false,
+            default: [
+                {
+                    name: "All",
+                    quantity: 0,
+                },
+            ],
         },
-      ],
+        mainCategory: {
+            type: mongoose.Schema.ObjectId,
+            ref: "MainCategory",
+        },
     },
-    mainCategory: {
-      type: mongoose.Schema.ObjectId,
-      ref: "MainCategory",
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 export default mongoose.model("Shop", shopSchema);
